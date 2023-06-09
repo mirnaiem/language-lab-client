1fa1c0c629fd5e5c74354559060145f7f3b9431d
mirnaiem <mirnaiem8teen@gmail.com>
2023-06-09 15:43:58 +0600

    add a class done

--- src/components/Pages/Dashboard/AddClass/AddClass.jsx
+++ src/components/Pages/Dashboard/AddClass/AddClass.jsx
@@ -1,12 +1,44 @@
 import React from 'react';
 import useAuthContext from '../../../../hooks/useAuthContext';
 import { useForm } from 'react-hook-form';
+import useAxiosSecure from '../../../../hooks/useAxiosSecure';
+import Swal from 'sweetalert2';
 
 const AddClass = () => {
  const {user}=useAuthContext()
+ const [axiosSecure]=useAxiosSecure()
  const { register, reset, handleSubmit, formState: { errors } } = useForm();
  const onSubmit = data => {
-console.log(data)
+  const className=data.name;
+  const photo=data.picture;
+  const email=data.email;
+  const instructorName=data.instructorName;
+  const price=data.price;
+  const seat=data.seat;
+  const saveClass={className,classImage:photo,instructorEmail:email, instructorName,price,seat};
+  console.log(saveClass);
+   fetch('http://localhost:3000/classes',{
+    method:'POST',
+    headers:{
+     'content-type': 'application/json'
+    },
+    body:JSON.stringify(saveClass)
+   })
+  .then(res=>res.json())
+  .then(data=>{
+   if(data.insertedId){
+    Swal.fire({
+     position: 'top-end',
+     icon: 'success',
+     title: 'User has been created successfully',
+     showConfirmButton: false,
+     timer: 1500
+   })
+   reset()
+   }
+  })
+
+
  }
  return (
   <div className='w-full'>
@@ -20,11 +52,11 @@ console.log(data)
           
         </div>
       
-       <div className="form-control">
+       <div className="">
           <label className="label">
             <span className="label-text">Class Image</span>
           </label>
-          <input type="text" {...register("picture", { required: true })} placeholder="photo" className="input input-bordered" />
+          <input type="file" {...register("picture", { required: true })} placeholder="photo"className="file-input file-input-bordered w-full max-w-xs"  />
         </div>
        
         <div className="form-control">
@@ -47,14 +79,14 @@ console.log(data)
           <label className="label">
             <span className="label-text">Available Seats</span>
           </label>
-          <input type="number" {...register("seat", { required: true })} defaultValue={'$'} placeholder='available seat' className="input input-bordered" />
+          <input type="number" {...register("seat", { required: true })}  placeholder='available seat' className="input input-bordered" />
           
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Price</span>
           </label>
-          <input type="number" {...register("price", { required: true })} defaultValue={'$'} placeholder='price' className="input input-bordered" />
+          <input type="number" {...register("price", { required: true })}  placeholder='price' className="input input-bordered" />
           
         </div>
        </div>
