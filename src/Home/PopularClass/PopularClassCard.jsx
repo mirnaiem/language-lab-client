01433ccabbe1ce80e0efbcc08255de4e851681a0
mirnaiem <mirnaiem8teen@gmail.com>
2023-06-12 10:04:40 +0600

    set scale in cls card

--- src/Home/PopularClass/PopularClassCard.jsx
+++ src/Home/PopularClass/PopularClassCard.jsx
@@ -4,7 +4,7 @@ const PopularClassCard = ({classInfo}) => {
  const {classImage,enroll}=classInfo
  return (
  <div>
-   <div className="card card-compact relative  duration-300   w-full bg-base-100 shadow-xl">
+   <div className="card card-compact relative  duration-300  hover:scale-105  w-full bg-base-100 shadow-xl">
   <figure><img className='w-full h-96 rounded-xl' src={classImage} alt="Shoes" /></figure>
   <h2 className='absolute top-4 left-4 text-xl font-bold'>Enroll Students: <span className='text-[#ef5743]'> {enroll}</span></h2>
 </div>
